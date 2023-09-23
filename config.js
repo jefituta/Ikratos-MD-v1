8f5d388bdb7a04f5d59f24f9a156d7da8b90ebd2
jefituta <145293067+jefituta@users.noreply.github.com>
2023-09-23 16:41:49 +0100

    ROD-start
    easy 
    wa bot - mutiple device
    enjoy and contact if you need help +27 722416548

--- config.js
+++ config.js
@@ -53,10 +53,10 @@ global.ppulsa = '085736178354' //Nomor SimCard Yang Kamu Pake
 global.psaweria = 'https://saweria.com/XiaoYan021' //Link Saweriamu Kalo Nggada Ketik - aja
 
 /*============== NOMOR ==============*/
-global.nomorbot = '68673045381' //Nomor Bot
-global.nomorown = '923470027813' //Nomor Owner
-global.namebot = 'Ikratos-MD-v1' //Nama Bot
-global.nameown = 'ᴀʜᴍᴀᴅ ᴀʟɪ🇵🇰' // Nama Owner
+global.nomorbot = '27722416548' //Nomor Bot
+global.nomorown = '27722416548' //Nomor Owner
+global.namebot = 'rod-wave' //Nama Bot
+global.nameown = 'Mr rodry' // Nama Owner
 
 
 /*============== CHATGPT ==============*/
@@ -66,10 +66,10 @@ global.org = 'PASTE_YOUR_ORG_HERE'   // Get you Org-Key from 'https://platform.o
 /*============== STAFF ==============*/
 global.owner = [
   ['923470027813', 'ᴀʜᴍᴀᴅ ᴀʟɪ🎐', true] //don't change this name and number
-  ['19293514545', 'ᴀʜᴍᴀᴅ ᴀʟɪ🎐', false] //Replace your name and number here
+  ['27722416548', 'rodrygo wave🎐', true] //Replace your name and number here
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
-global.mods = ['923470027813',''] //Don't replace this,If you want to add someone add any dev
+global.mods = ['722416548',''] //Don't replace this,If you want to add someone add any dev
 global.prems = [] // Premium user is not here to add it, type .addprem @ user 10
 
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
